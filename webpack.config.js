198e0e1e4fc605b64879ed9ab95da652d394b130
Duy Khanh <duykhanh@Duys-MacBook-Pro.local>
2018-07-11 14:44:44 +0700

    first setup project

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: [
+      './src/index.js'
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader']
+          }
+        ]
+      },
+      resolve: {
+        extensions: ['*', '.js', '.jsx']
+      },
+    output: {
+      path: path.join(__dirname, 'dist'),
+      publicPath: '/',
+      filename: 'bundle.js'
+    },
+    devServer: {
+      contentBase: './dist'
+    }
+  };
